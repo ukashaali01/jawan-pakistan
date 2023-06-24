6316d7f0b568927c44ab2a60778c886d353941ac
syed ukasha ali <ukasha477@gmail.com>
2023-06-24 23:20:55 +0500

    3rd

---
+++ bin/3rd.dart
@@ -0,0 +1,16 @@
+void main() {
+  List<String> days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
+
+  while (days.isNotEmpty) {
+    String removedDay = days.removeLast();
+    print('Removed day: $removedDay');
+  }
+}
